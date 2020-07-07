c5d5273c51098854c4e62e3cbfa87dbb6b0b8631
leon <leoncyao@gmail.com>
2020-07-07 13:10:46 -0700

    adding 5_2-A html

--- KhT/examples/5_2-A.py
+++ KhT/examples/5_2-A.py
@@ -2,7 +2,7 @@ name = "5_2-A"
 tangle = "cap1.cap2.neg1.neg0.neg0.neg1.neg1.neg0.neg0.neg1.neg1.neg1.neg0.neg0.pos2.cup1"
 Tangle = Tangle(tangle)
 
-figured_out_tangle = False
+figured_out_tangle = True
 
 html_content=""
 
