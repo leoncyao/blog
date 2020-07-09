222f13ddb72d0175d848484b5aaf13ff6b41c3b7
leoncyao <leoncyao@gmail.com>
2020-07-09 14:08:48 -0700

    5_1 is different?

--- KhT/KhT/__main__.py
+++ KhT/KhT/__main__.py
@@ -23,7 +23,7 @@ def main(data):
     
 if __name__ == "__main__":
     filename=sys.argv[1]
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         filepath="examples/" + sys.argv[2]
     else:
         filepath="examples/"+filename
