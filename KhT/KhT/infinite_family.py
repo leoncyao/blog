ef29da6a4b73319292743787c7130eff7d77b369
leon <leoncyao@gmail.com>
2020-07-12 01:25:11 -0700

    worked upto 3rd

--- KhT/KhT/infinite_family.py
+++ KhT/KhT/infinite_family.py
@@ -3,7 +3,7 @@ import KhT
 import os
 if __name__ == "__main__":
 	t="cap0.neg1.cap3.neg2.neg2.neg1.cup1"
-	for i in range(1,10):
+	for i in range(1,11):
 		new_t = t[:-5] + ".pos0" * i + ".cup1"
 		print(new_t)
 		filename = "asimov_" + str(i)
