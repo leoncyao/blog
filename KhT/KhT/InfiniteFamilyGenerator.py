71c6ebbd3b1339df8e1ca5ca203fe276702ba194
leoncyao <leoncyao@gmail.com>
2020-08-04 12:14:13 -0700

    want to compile 6_2-A family

--- KhT/KhT/InfiniteFamilyGenerator.py
+++ KhT/KhT/InfiniteFamilyGenerator.py
@@ -28,11 +28,11 @@ if __name__ == "__main__":
 	# main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 	# main("rational_experiment_1", "neg0", 5, "rational_experiment")
 
-	main("3_1-family_1", "pos0", 5, "3_1-family")
+	# main("3_1-family_1", "pos0", 5, "3_1-family")
 
-	main("4_1-A-family_1", "neg3.neg2.neg2.neg3", 3, "4_1-A-family",num_iterations=5)
+	# main("4_1-A-family_1", "neg3.neg2.neg2.neg3", 3, "4_1-A-family",num_iterations=5)
 
-	main("4_1-B-family_1", "pos0", 13, "4_1-B-family",num_iterations=5)
+	# main("4_1-B-family_1", "pos0", 13, "4_1-B-family",num_iterations=5)
 
 	# main("5_2-A-family_1", "neg3.neg2.neg2.neg3", 3, "5_2-A-family",num_iterations=5)
 
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 
 	# main("5_2-B_1", "pos1.pos0.pos0.pos1", 2, "5_2-B",num_iterations=5)
 
-	
+	main("6_2-A-family_1", "neg3.neg2.neg2.neg3", 3, "6_2-A-family")
 
 	# main("galois_1", "pos0", 12, "galois",num_iterations=5)
 
