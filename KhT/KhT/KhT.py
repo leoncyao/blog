39e692258ab87dc76e837539f7254404fcdbb9e8
leoncyao <leoncyao@gmail.com>
2020-08-11 02:49:32 -0700

    compiling

--- KhT/KhT/KhT.py
+++ KhT/KhT/KhT.py
@@ -80,6 +80,7 @@ def asdf(tangle_name,tangle_path="miscellaneous", resultingdirectory=None):
     multicurve.save(name)
     print("name is " + name)
     test = []
+    print(multicurve.comps)
     for comp in multicurve.comps:
         # print("new comp")
         # print(comp.is_looptype())
@@ -88,6 +89,7 @@ def asdf(tangle_name,tangle_path="miscellaneous", resultingdirectory=None):
                 # should not be a cycle
                 test.append(comp)
     multicurve.comps = test
+    print(multicurve.comps)
     html_content += multicurve.html(name,"_BNr7","hdelta",Tangle)
 
 
