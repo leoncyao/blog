906f0bf02c6095577700d169176c9b0ee9e6787b
leoncyao <leoncyao@gmail.com>
2020-08-11 10:31:05 -0700

    compiling

--- KhT/KhT/InfiniteFamilyGenerator.py
+++ KhT/KhT/InfiniteFamilyGenerator.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 	
 	# main("copernicus_1", "pos0", 12, "copernicus",num_iterations=5)
 
-	main("5_2-B_1", "pos1.pos0.pos0.pos1", 2, "5_2-B",num_iterations=5)
+	main("5_2-B_1", "pos1", 10, "5_2-B",num_iterations=5)
 
 	# main("6_2-A-family_1", "neg3.neg2.neg2.neg3", 3, "6_2-A-family")
 
