8d854e1c944ab72f58a2fa331c5ececc5d8dd7a3
leoncyao <leoncyao@gmail.com>
2020-08-11 01:45:54 -0700

    ohgodlol

--- _site/KhT/KhT/BNComplexes.py
+++ _site/KhT/KhT/BNComplexes.py
@@ -528,7 +528,7 @@ class multicurve(object):
             # html_row+="<span onclick=on('"+filename+"/"+longname+pdf_suffix+"')><img class="+classname+" src='"+filename+"/"+longname+"_thumbs-"+png_suffix+".png'></span>\n"
             # html_row+="<span onclick=on('" + filepath[12:] + longname+pdf_suffix+"')><img class="+classname+" src='" + filepath[12:] + longname+"_thumbs-"+png_suffix+".png'></span>\n"    
             # html_row+="<span onclick=on('KhT/examples/" + filepath[12: ] + longname+pdf_suffix+"')><img class="+classname+" src='KhT/examples/" + filepath[12: ] + longname+"_thumbs-"+png_suffix+".png'></span>\n"
-            html_row+="<span onclick=on('KhT/examples/" + filepath[12: ] + longname+pdf_suffix+"')><img class="+classname+" src='KhT/examples/" + filepath[12: ] + longname+"_thumbs-"+png_suffix+".png'></span>\n"    
+            html_row+="<span onclick=on('KhT/examples/" + filepath[12: ] + longname+pdf_suffix+"')><img class="+classname+" src='../KhT/examples/" + filepath[12: ] + longname+"_thumbs-"+png_suffix+".png'></span>\n"    
         html_row+="\n</p>"
         return html_row
         
